8f773662f5fc03da025577068235025bb37b25f9
Gabe O'Leary <golear@microsoft.com>
2016-04-24 15:08:13 -0700

    Added sit/stand icon

--- app/scripts/background.ts
+++ app/scripts/background.ts
@@ -9,13 +9,11 @@ chrome.runtime.onInstalled.addListener(details => {
     console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({ text: '\'Allo' });
-
 chrome.alarms.onAlarm.addListener(function (alarm) {
     if (alarm.name === ALARM_NAME) {
         chrome.notifications.create({
             type: 'basic',
-            iconUrl: 'images/icon-38.png',
+            iconUrl: 'images/sitting-icon-38.png',
             title: `test notification from: ${new Date()} number: ${count++}`,
             message: 'test message',
             buttons: [
