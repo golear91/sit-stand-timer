b02a40cd3a9bc1e3ce764cc8b138964e368d7909
Gabe O'Leary <golear@microsoft.com>
2016-04-26 11:04:43 -0700

    added idle notification and updated dependencies

--- app/scripts/background.ts
+++ app/scripts/background.ts
@@ -5,6 +5,8 @@ const NOTIFICATION_FREQUENCY_MINUTES: number = 1;
 let count: number = 0;
 
 const STARTUP_NOTIFICATION_ID: string = 'sit-stand-startup';
+const IDLE_STATE_NOTIFICATION_ID: string = 'sit-stand-idle';
+const STANDARD_NOTIFICATION_ID: string = 'sit-stand-standard';
 
 let sitting: boolean = true;
 
@@ -14,9 +16,10 @@ let standupNotification = {
     title: `It's time to stand up!`,
     message: 'Click ',
     buttons: [
-        { title: "Done" }
+        { title: "Done" },
+        { title: "Snooze" }
     ],
-    isClickable: true
+    requireInteraction: true
 }
 
 let sitDownNotification = {
@@ -25,10 +28,28 @@ let sitDownNotification = {
     title: `It's time to sit down`,
     message: 'message',
     buttons: [
-        { title: "Done" }
+        { title: "Done" },
+        { title: "Snooze" }
     ],
-    isClickable: true
+    requireInteraction: true
 }
+
+chrome.idle.onStateChanged.addListener(function (newState) {
+    console.log("idle newState:" + newState);
+    chrome.notifications.clear(STANDARD_NOTIFICATION_ID);
+    if (newState === 'active') {
+        chrome.notifications.create(IDLE_STATE_NOTIFICATION_ID, {
+            type: 'basic',
+            iconUrl: 'images/sitting-icon-38.png',
+            title: 'Sit Stand Timer idle change',
+            message: 'Are you sitting or standing right now?',
+            buttons: [
+                { title: 'Sitting' },
+                { title: 'Standing' }
+            ]
+        });
+    }
+});
 
 chrome.runtime.onStartup.addListener(function () {
     console.log("onStartup");
@@ -46,13 +67,22 @@ chrome.runtime.onStartup.addListener(function () {
 
 chrome.notifications.onButtonClicked.addListener(function (notificationId, buttonIndex) {
     console.log(`Button ${buttonIndex} clicked on notification ${notificationId}`);
-    if (notificationId === STARTUP_NOTIFICATION_ID) {
+    if (notificationId === STARTUP_NOTIFICATION_ID || notificationId === IDLE_STATE_NOTIFICATION_ID) {
         sitting = buttonIndex === 0;
-    } else {
+    } else if (buttonIndex === 0) {
         sitting = !sitting;
+        if (sitting) {
+            chrome.browserAction.setIcon({ path: 'images/sitting-icon-38.png' });
+        }
+        else {
+            chrome.browserAction.setIcon({ path: 'images/standing-icon-38.png' });
+        }
     }
     chrome.notifications.clear(notificationId);
+});
 
+chrome.idle.onStateChanged.addListener(function (newState) {
+    console.log(`idle state changed to ${newState}`);
 });
 
 chrome.runtime.onInstalled.addListener(details => {
@@ -63,10 +93,10 @@ chrome.alarms.onAlarm.addListener(function (alarm) {
     console.log("ALARM");
     if (alarm.name === ALARM_NAME) {
         if (sitting) {
-            chrome.notifications.create(standupNotification);
+            chrome.notifications.create(STANDARD_NOTIFICATION_ID, standupNotification);
         }
         else {
-            chrome.notifications.create(sitDownNotification);
+            chrome.notifications.create(STANDARD_NOTIFICATION_ID, sitDownNotification);
         }
     }
 });
